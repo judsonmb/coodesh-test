5ad0ba8aa9432d7aaa835e7ff36ec2a11fcf6961
Judson Bandeira <judsonmelobandeira@gmail.com>
2025-03-14 17:59:13 -0300

    First commit.

---
+++ open-food/database/migrations/2025_03_14_204308_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable()->unique();
+            $table->enum('status', ['draft', 'trash', 'published'])->nullable();
+            $table->datetime('imported_t')->nullable(); 
+            $table->string('url')->nullable();
+            $table->string('creator')->nullable();
+            $table->integer('created_t')->nullable();
+            $table->integer('last_modified_t')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('brands')->nullable();
+            $table->string('categories')->nullable();
+            $table->string('labels')->nullable();
+            $table->string('cities')->nullable();
+            $table->string('purchase_places')->nullable();
+            $table->string('stores')->nullable();
+            $table->text('ingredients_text')->nullable();
+            $table->text('traces')->nullable();
+            $table->string('serving_size')->nullable();
+            $table->decimal('serving_quantity', 10, 2)->nullable();
+            $table->integer('nutriscore_score')->nullable();
+            $table->string('nutriscore_grade')->nullable();
+            $table->string('main_category')->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
